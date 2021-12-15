3b468558583b6f97496285321aad1088852a2a7e
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2021-12-15 00:31:32 -0800

    start

---
+++ index.js
@@ -0,0 +1,31 @@
+const createEmployee = () => {
+ inquirer.prompt([
+  {
+   type: 'list',
+   name: 'choice',
+   message: 'What would you like to do?',
+   choices: ['View Employee', 'View Department', 'View Employees Roles', 'View Employees By Manager', 'Add Employee', 'Remove Employee', 'Update Employee Roles', 'Update Employee Manager', 'View All Roles'],
+  },
+ ])
+  .then(answer =>{
+   if (answer.choice === 'Add Employee') {
+   inquirer.prompt([
+  {
+   type: 'input',
+   name: 'firstname',
+   message: 'Type your employee first name',
+  },
+  {
+   type: 'input',
+   name: 'lastname',
+   message: 'Type your employee last name',
+  },
+  {
+   type: 'list',
+   name: 'role',
+   message: 'Pick a role for your employee',
+   choices: ['Intern', 'Engineer', 'Manager'],
+  }
+  ])
+ }
+}) 
